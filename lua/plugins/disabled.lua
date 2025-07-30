4907805112f165419ac48ea88dbac354484a95fa
Ronnie Jin <ronjin@amazon.com>
2025-07-30 16:31:50 -0700

    chore: disable go and ts lsp for mason for now, disabled blink.cmp in the config bc error
    Error detected while processing VimEnter Autocommands for "*":
    Error executing lua callback: vim/_editor.lua:0: VimEnter Autocommands
    for "*"..script nvim_exec2() called at VimEnter Autocommands for "*":0,
    line 1: Vim(iunmap):E31: No such mapping
    stack traceback:
            [C]: in function 'nvim_exec2'
            vim/_editor.lua: in function 'cmd'
            /Users/ronjin/.config/nvim/lua/plugins/disabled.lua:13: in
    function </Users/ronjin/.config/nvim/lua/plugins/disabled.lua:12>

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,17 +3,17 @@ return {
     "echasnovski/mini.ai",
     enabled = false,
   },
-  {
-    -- This can be in any file that gets loaded after plugin initialization
-    "Saghen/blink.cmp",
-    config = function()
-      -- Add this to run after the plugin is loaded
-      vim.api.nvim_create_autocmd("VimEnter", {
-        callback = function()
-          vim.cmd("iunmap <CR>")
-        end,
-        once = true,
-      })
-    end,
-  },
+  -- {
+  --   -- This can be in any file that gets loaded after plugin initialization
+  --   "Saghen/blink.cmp",
+  --   config = function()
+  --     -- Add this to run after the plugin is loaded
+  --     vim.api.nvim_create_autocmd("VimEnter", {
+  --       callback = function()
+  --         vim.cmd("iunmap <CR>")
+  --       end,
+  --       once = true,
+  --     })
+  --   end,
+  -- },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,7 +1,34 @@
+-- return {
+--   "neovim/nvim-lspconfig",
+--   opts = {
+--     servers = {
+--       -- Disable Go LSP
+--       gopls = false,
+--
+--       -- Disable TypeScript LSP
+--       tsserver = false,
+--       -- or if you're using the newer name:
+--       ts_ls = false,
+--     },
+--   },
+--   init = function()
+--     local keys = require("lazyvim.plugins.lsp.keymaps").get()
+--     keys[#keys + 1] = { "K", "5k" }
+--   end,
+-- }
+
 return {
   "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      -- Disable the problematic servers
+      gopls = false,
+      tsserver = false,
+      ts_ls = false,
+    },
+  },
   init = function()
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    keys[#keys + 1] = { "K", "5k" }
+    -- Remove the K mapping from LSP keymaps
+    vim.keymap.set("n", "K", "5k", { desc = "Move 5 lines up" })
   end,
 }
